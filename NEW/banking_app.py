16d54800b9764d6fbd7c756e19bec21b520706d7
Julian <juliancastro320@gmail.com>
2022-06-04 12:58:39 -0500

    Se añadio la funcion de agregar y quitar dinero de la cuenta

--- NEW/banking_app.py
+++ NEW/banking_app.py
@@ -13,7 +13,7 @@ Vamos a tener dos clases de usuarios:
 """
 
 #Clase padre
-from curses.ascii import US
+
 
 
 class User():
@@ -59,5 +59,35 @@ class Bank(User):
         #Debemos traer lo que traia la clase padre con el metodo superinit
         super().__init__(name, age, gender)
 
-        
+        #Cada usuario tendrá un balance por defecto de 0
+        self.balance = 0
+
+
+    def deposit(self, amount):
+        """
+        Esta funcion le permite al usuario depositar dinero a su cuenta
+        """
+
+        self.ammount = amount
+        self.balance = self.balance + amount
+
+        print("Account Balance Has Been Updated, your new balance is: ", self.balance)
+
+
+    def withdraw(self, amount):
+        """
+        Esta funcion le permite retirar dinero de su cuenta
+        """
+
+        self.amount = amount
+
+        #Comprobamos que hay suficientes fondos
+        if self.amount > self.balance:
+            print("Insufficient Funds | Balance Available: ", self.balance)
+
+        else:
+            #Actualizamos el balance
+            self.balance = self.balance - self.amount
+            print("Account balance has been updated: ", self.balance)
+
 
